03a414e4a3319d7017b96d4fde6f550933d60843
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-02 10:47:17 +0700

    first commit

--- GuessTheseMovie/ContentView.swift
+++ GuessTheseMovie/ContentView.swift
@@ -6,16 +6,73 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Guess These Movies")
+                .fontWeight(.bold)
+                .font(.system(.title, design: .rounded))
+            
+            Text("Can you guess the movie from these emojis?\n Tap the button to find out the answers")
+                .multilineTextAlignment(.center)
+            
+            Button {
+                speak(text: "I don't know")
+                
+            } label: {
+                Text("👦🏻 👴🏻 🚗 ⌚️")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .background(Color.green)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "Frozen")
+            } label: {
+                Text("🏰 👭 🌀 ❄️ ⛄️")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .background(Color.yellow)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "Ocean Eleven")
+            } label: {
+                Text("🌊 1️⃣ 1️⃣")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "Spider Man")
+            } label: {
+                Text("🕷 🏃")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .background(Color.brown)
+            .cornerRadius(20)
+            
         }
-        .padding()
+    }
+    
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
     }
 }
 
