cb6ea82a4ce20f77e2bfeaf8da5e76730f0a0623
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-02 09:42:30 +0700

    Initial Commit

---
+++ GuessTheseMovie/GuessTheseMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheseMovieApp.swift
+//  GuessTheseMovie
+//
+//  Created by Benny Rinaldo on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheseMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
